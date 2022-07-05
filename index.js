407df807e75879d8b7c05076afeee424851217fb
Logan Lampton <lampton.logan@gmail.com>
2022-07-05 19:45:19 -0400

    commit tree cee9ab5b1549433b359af4c74f8b5e375e4dd648

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(stringName, eventName) {
+    const newArray = [];
+    for (let i = 0; i < stringName.length; i++) {
+        newArray.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray;
+};
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+};
